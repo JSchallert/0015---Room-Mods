497d8543e1cac069d3c2cef1320e93349be84fde
hmc-yak-s18 <jschallert@cs.hmc.edu>
2018-04-09 23:06:14 -0700

    added some extra functions

--- ScreenControl/ScreenControl.ino
+++ ScreenControl/ScreenControl.ino
@@ -3,31 +3,71 @@
 //Date: 4/8/2018
 
 #include "LowPower.h"
-//testing github functionality
 #define DIR 5
 #define STEP 4
+#define TRIGGER 6
+int stepsPerRot = 200;
+bool deployed = false;
+bool stepDirection = true;
+bool stepping = false;
+int totalSteps = 1000;
+int brokenSteps = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(38400);
   pinMode(DIR,OUTPUT);
   pinMode(STEP,OUTPUT);
-  digitalWrite(STEP,HIGH);
-}
+  pinMode(TRIGGER,INPUT);
+  digitalWrite(STEP,stepDirection);}
 
 void reverse(){
-  digitalWrite(DIR,HIGH);
-  delay(200);
-  //digitalWrite(DIR,LOW);
-}
-
-void stepperStep(){
+  digitalWrite(DIR,!stepDirection);
+  stepDirection = !stepDirection;}
+  
+void stepCustom(float timeDelay){
+  stepping = true;
   digitalWrite(STEP,LOW);
-  delay(10);
+  delay(timeDelay);
   digitalWrite(STEP,HIGH);
-  delay(10);
+  delay(timeDelay);
+  if(stepDirection){
+    brokenSteps+=1;
+  }else{
+    brokenSteps-=1;}
 }
 
+void screenDeploy(bool stepdir){
+  float timescale;
+  for(int i=0,i<totalSteps,i++){
+    timescale = sqrt(totalSteps);
+    stepCustom(timescale);
+    if(TRIGGER){
+      Serial.println("Stopped!");
+      break;}
+  }
+  stepping = false;
+  Serial.println("Resetting...");
+  Serial.println(brokenSteps);
+  delay(250);
+}
+
+void zero(int steps){
+  reverse();
+  if(!stepDirection){
+    for (int i = 0;i<steps;i++){
+      stepCustom(4);}
+  }else{
+    for (int i = steps;i!=totalSteps;i++){
+      stepCustom(4);}}
+}
 
 void loop() {
-  stepperStep();
+  if(TRIGGER){
+    if(brokenSteps != 0){
+      zero(brokenSteps);}
+    else if(deployed){
+      screenDeploy(deployed);}
+    else{
+      screenDeploy(deployed);}
+  }
 }
